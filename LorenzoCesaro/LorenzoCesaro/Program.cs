cf34d55a600735be8e7c41616e38e87f41f7c446
Cesarolorenzo <98150960+Cesarolorenzo@users.noreply.github.com>
2022-01-28 10:10:43 +0100

    aggiunto il saluto richiesta nome

---
+++ LorenzoCesaro/LorenzoCesaro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LorenzoCesaro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dimmi il tuo nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("ciao" + nome);
+        }
+    }
+}
